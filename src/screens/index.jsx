3174f08cb88b44919ef8e279b1e613c56c07e573
M Abrar Khalid <abrarprince471@gmail.com>
2022-10-19 17:46:44 +0500

    map fixes

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -9,7 +9,7 @@ export default function index() {
         <div className="luxury__section">
           <div className="luxury__section__left">
             <div className="luxury__section__left__heading">
-         Our luxury  <span>upcoming</span> for more discering Customers
+              Our luxury <span>upcoming</span> for more discering Customers
             </div>
             <div className="luxury__section__left__para">
               Dream Home is a gated community with a great location. Located
@@ -18,21 +18,30 @@ export default function index() {
               with easy access to freeways, buses and trolleys. . Laundry is
               available on premises.
             </div>
-            <div className="luxury__section__left__button">
-            Read More
-            </div>
-
+            <div className="luxury__section__left__button">Read More</div>
           </div>
           <div className="luxury__section__right">
-          <div className="luxury__section__right__img">
-            <img className="luxury__section__right__image" src={luxury} alt="" />
-          </div>
-          <div className="luxury__section__right__imgs">
-            <img className="luxury__section__right__image" src={luxury} alt="" />
-          </div>
-          <div className="luxury__section__right__imgss">
-            <img className="luxury__section__right__image" src={luxury} alt="" />
-          </div>
+            <div className="luxury__section__right__img">
+              <img
+                className="luxury__section__right__image"
+                src={luxury}
+                alt=""
+              />
+            </div>
+            <div className="luxury__section__right__imgs">
+              <img
+                className="luxury__section__right__image"
+                src={luxury}
+                alt=""
+              />
+            </div>
+            <div className="luxury__section__right__imgss">
+              <img
+                className="luxury__section__right__image"
+                src={luxury}
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -48,7 +57,17 @@ export default function index() {
           </div>
 
           <div className="location__Section__img">
-            <img className="location__Section__image" src={map} alt="" />
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1208.3247925047367!2d72.95992991031669!3d31.06360773507335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3922f1a03288dd97%3A0xd774a67d9fa85858!2sCrazy%20Beings!5e0!3m2!1sen!2s!4v1666182994553!5m2!1sen!2s"
+              loading="lazy"
+              style={{
+                width: "100%",
+                height: 500,
+                border: "none",
+                margin: "4em 0em",
+              }}
+              referrerpolicy="no-referrer-when-downgrade"
+            />
           </div>
         </div>
       </div>
