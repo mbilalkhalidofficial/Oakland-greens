b2310b352532d33a59c1eefd9efc7a8cd608df77
M Abrar Khalid <abrarprince471@gmail.com>
2022-10-19 20:44:00 +0500

    farm section done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ import aboutlast from "../assets/aboutlast.png";
 import oaklandlogo from "../assets/oaklandlogo.png";
 import map from "../assets/map.svg";
 import map2 from "../assets/map2.svg";
+import farmbackground from "../assets/farm-background.png";
 
 import first from "../assets/first.svg";
 import first2 from "../assets/first2.svg";
@@ -350,6 +351,236 @@ export default function Header() {
           </div>
         </div>
       </div> */}
+      <section className="Form__House">
+        <div className="farm__background">
+          <img src={farmbackground} alt="farmbackground " />
+        </div>
+        <div className="farm__house__content__overlay">
+          <div className="farm__house__content__heading">
+            Farm House Feature
+          </div>
+          <div className="farm__house__content__part__one">
+            <div className="farm__house__content__one">
+              <div className="farm__house__content__one__svg">
+                <svg
+                  width="71"
+                  height="59"
+                  viewBox="0 0 71 59"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3.39062 3.68311H67.8924M3.39062 55.8988H67.8924M35.6415 3.68311V55.8988M49.4633 23.6479V35.934M7.99789 3.68311H63.2851V55.8988H7.99789V3.68311Z"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M44.8565 28.2552L49.4638 23.6479L54.071 28.2552M21.8202 35.934V23.6479M21.8202 35.934L17.2129 31.3267M21.8202 35.934L26.4274 31.3267"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="farm__house__content__one__heading">Elevator</div>
+            </div>
+            <div className="farm__house__content__two">
+              <div className="farm__house__content__one__svg">
+                <svg
+                  width="71"
+                  height="59"
+                  viewBox="0 0 71 59"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3.39062 3.68311H67.8924M3.39062 55.8988H67.8924M35.6415 3.68311V55.8988M49.4633 23.6479V35.934M7.99789 3.68311H63.2851V55.8988H7.99789V3.68311Z"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M44.8565 28.2552L49.4638 23.6479L54.071 28.2552M21.8202 35.934V23.6479M21.8202 35.934L17.2129 31.3267M21.8202 35.934L26.4274 31.3267"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="farm__house__content__one__heading">Elevator</div>
+            </div>
+            <div className="farm__house__content__three">
+              <div className="farm__house__content__one__svg">
+                <svg
+                  width="71"
+                  height="59"
+                  viewBox="0 0 71 59"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3.39062 3.68311H67.8924M3.39062 55.8988H67.8924M35.6415 3.68311V55.8988M49.4633 23.6479V35.934M7.99789 3.68311H63.2851V55.8988H7.99789V3.68311Z"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M44.8565 28.2552L49.4638 23.6479L54.071 28.2552M21.8202 35.934V23.6479M21.8202 35.934L17.2129 31.3267M21.8202 35.934L26.4274 31.3267"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="farm__house__content__one__heading">Elevator</div>
+            </div>
+            <div className="farm__house__content__four">
+              <div className="farm__house__content__one__svg">
+                <svg
+                  width="71"
+                  height="59"
+                  viewBox="0 0 71 59"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3.39062 3.68311H67.8924M3.39062 55.8988H67.8924M35.6415 3.68311V55.8988M49.4633 23.6479V35.934M7.99789 3.68311H63.2851V55.8988H7.99789V3.68311Z"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M44.8565 28.2552L49.4638 23.6479L54.071 28.2552M21.8202 35.934V23.6479M21.8202 35.934L17.2129 31.3267M21.8202 35.934L26.4274 31.3267"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="farm__house__content__one__heading">Elevator</div>
+            </div>
+          </div>
+          <div className="farm__house__content__part__two">
+            <div className="farm__house__content__five">
+              <div className="farm__house__content__one__svg">
+                <svg
+                  width="71"
+                  height="59"
+                  viewBox="0 0 71 59"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3.39062 3.68311H67.8924M3.39062 55.8988H67.8924M35.6415 3.68311V55.8988M49.4633 23.6479V35.934M7.99789 3.68311H63.2851V55.8988H7.99789V3.68311Z"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M44.8565 28.2552L49.4638 23.6479L54.071 28.2552M21.8202 35.934V23.6479M21.8202 35.934L17.2129 31.3267M21.8202 35.934L26.4274 31.3267"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="farm__house__content__one__heading">Elevator</div>
+            </div>
+            <div className="farm__house__content__six">
+              <div className="farm__house__content__one__svg">
+                <svg
+                  width="71"
+                  height="59"
+                  viewBox="0 0 71 59"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3.39062 3.68311H67.8924M3.39062 55.8988H67.8924M35.6415 3.68311V55.8988M49.4633 23.6479V35.934M7.99789 3.68311H63.2851V55.8988H7.99789V3.68311Z"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M44.8565 28.2552L49.4638 23.6479L54.071 28.2552M21.8202 35.934V23.6479M21.8202 35.934L17.2129 31.3267M21.8202 35.934L26.4274 31.3267"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="farm__house__content__one__heading">Elevator</div>
+            </div>
+            <div className="farm__house__content__seven">
+              <div className="farm__house__content__one__svg">
+                <svg
+                  width="71"
+                  height="59"
+                  viewBox="0 0 71 59"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3.39062 3.68311H67.8924M3.39062 55.8988H67.8924M35.6415 3.68311V55.8988M49.4633 23.6479V35.934M7.99789 3.68311H63.2851V55.8988H7.99789V3.68311Z"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M44.8565 28.2552L49.4638 23.6479L54.071 28.2552M21.8202 35.934V23.6479M21.8202 35.934L17.2129 31.3267M21.8202 35.934L26.4274 31.3267"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="farm__house__content__one__heading">Elevator</div>
+            </div>
+            <div className="farm__house__content__eight">
+              <div className="farm__house__content__one__svg">
+                <svg
+                  width="71"
+                  height="59"
+                  viewBox="0 0 71 59"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M3.39062 3.68311H67.8924M3.39062 55.8988H67.8924M35.6415 3.68311V55.8988M49.4633 23.6479V35.934M7.99789 3.68311H63.2851V55.8988H7.99789V3.68311Z"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M44.8565 28.2552L49.4638 23.6479L54.071 28.2552M21.8202 35.934V23.6479M21.8202 35.934L17.2129 31.3267M21.8202 35.934L26.4274 31.3267"
+                    stroke="#B29C3F"
+                    stroke-width="6.14302"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+              <div className="farm__house__content__one__heading">Elevator</div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
